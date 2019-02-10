42ac0cd0b80982f05dd01e394ed0cad75b6c909b
Brandon Rivera Godines <brandonriveragodines@hotmail.com>
2019-02-09 20:18:51 -0600

    Commit 08/02/2019 : 20:20
    Se vuelca el proyecto dentro del repositorio, se espera solo depende de dos iteraciones.

---
+++ login/index.php
@@ -0,0 +1,32 @@
+<?php
+    require_once '../include/DB_Functions.php';
+    $db = new DB_Functions();
+
+    $response = array("error" => FALSE);
+
+    if(isset($_POST['email']) && isset($_POST['password'])){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $user = $db->getUserByEmailAndPassword($email, $password);
+        if($user != FALSE){
+            $response["error"] = FALSE;
+            $response["uid"] = $user["unique_id"];
+            $response["user"]["name"] = $user["name"];
+            $response["user"]["email"] = $user["email"];
+            $response["user"]["created_at"] = $user["created_at"];
+            $response["user"]["updated_at"] = $user["updated_at"];
+            $response["user"]["login_status"] = $user["status_login"];
+            $db->setStatusOn($email);
+            echo json_encode($response);
+        } else {
+            $response["error"] = TRUE;
+            $response["error_msg"] = "Login credentials are wrong. Please try again.";
+            echo json_encode($response);
+        }
+    } else {
+        $response["error"] = TRUE;
+        $response["error_msg"] = "Required parameters email or password is missing!";
+        echo json_encode($response);
+    }
+?>
---
+++ query/login_api.sql
@@ -0,0 +1,45 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.9
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3306
+-- Tiempo de generación: 10-02-2019 a las 02:16:38
+-- Versión del servidor: 5.7.21
+-- Versión de PHP: 7.0.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `login_api`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `unique_id` varchar(23) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `encrypted_password` varchar(80) NOT NULL,
+  `salt` varchar(10) NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `updated_at` datetime DEFAULT NULL,
+  `status_login` tinyint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_id` (`unique_id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
